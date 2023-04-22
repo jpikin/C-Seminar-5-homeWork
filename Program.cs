330dd96b6d0f23e5d5312d5290a901e35e0307b8
Evgeniy Pikin <pikin@bk.ru>
2023-04-22 23:28:58 +0500

    Task 36 added

--- Program.cs
+++ Program.cs
@@ -2,6 +2,35 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
+// void GetResult(int[] arr, int count)
+// {
+//     Console.WriteLine($"{"["}{String.Join(", ", arr)}{"]"} -> {count}");
+// }
+
+// int GetRandNum(int a, int b)
+// {
+//     int num = new Random().Next(a, b);
+//     return num;
+// }
+
+// int [] arrNum = new int [GetRandNum(5, 10)];
+// int counter = 0;
+// for (int i = 0; i<arrNum.Length; i++)
+// {
+//     arrNum[i] = GetRandNum(100, 1000);
+//     if (arrNum[i] % 2 == 0)
+//     {
+//         counter++;
+//     }
+// }
+// GetResult(arrNum, counter);
+
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+//  Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
 void GetResult(int[] arr, int count)
 {
     Console.WriteLine($"{"["}{String.Join(", ", arr)}{"]"} -> {count}");
@@ -16,19 +45,15 @@ int GetRandNum(int a, int b)
 int [] arrNum = new int [GetRandNum(5, 10)];
 int counter = 0;
 for (int i = 0; i<arrNum.Length; i++)
-{
-    arrNum[i] = GetRandNum(100, 1000);
-    if (arrNum[i] % 2 == 0)
-    {
-        counter++;
-    }
+{ 
+   arrNum[i] = GetRandNum(1, 1000);
+   if (i % 2 !=0)
+   {
+    counter += arrNum[i];
+   } 
 }
 GetResult(arrNum, counter);
 
-
-// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
-// [3, 7, 23, 12] -> 19
-// [-4, -6, 89, 6] -> 0
 
 // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
